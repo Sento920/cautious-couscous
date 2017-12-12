1a043df1763140a44fc5eda6e319d5f8d8e1b44a
Tyler Rusch <sentokun9200@hotmail.com>
2017-12-12 17:39:15 -0600

    Added Collectibles skeleton

---
+++ couscous/Assets/Scripts/Objects/Collectible.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectible : MonoBehaviour {
+
+	Collider2D collectorArea;
+	Rigidbody2D rigidbody2D;
+
+
+	// Use this for initialization
+	void Start () {
+		rigidbody2D = GetComponent<Rigidbody2D>();
+		collectorArea = GetComponent<Collider2D>();
+	}
+
+	void OnTriggerEnter2D(Collider other){
+		//Destroy ourselves, and re-attach to the Player.
+		if(other.gameObject.transform.tag == "player"){
+			Debug.Log("Player touch");
+		} else {
+			Debug.Log("Non-Player Touch");
+		}
+	}
+
+}
